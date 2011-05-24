0c33e1bb3f84ffab7bf715916264a06fb45f9569
Stijn Debrouwere <stijn@stdout.be>
2011-05-24 20:05:27 +0200

    Install script works as intended.

--- install/install.js
+++ install/install.js
@@ -10,8 +10,12 @@ stdin.setEncoding('utf8');
 
 if (os == 'Darwin') {
     var default_web_root = process.env.HOME + "/Sites";
-} else {
+    var init_script = '/org.draughtsman.plist';
+    var init_dir = process.env.HOME + "/Library/LaunchAgents";
+} else if (os == 'Linux') {
     var default_web_root = "/var/www";
+    var init_script = '/draughtsman.conf';
+    var init_dir = "/etc/init";
 }
 
 /* logic */
@@ -28,7 +32,12 @@ function configure(answers) {
     var run_on_startup = (answers[0].indexOf('n') === -1);
     var web_root = answers[1] || default_web_root;
     if (run_on_startup) {
-        stdout.write("Next time you reboot, Draughtsman will be at your service at port 3400, processing files in " + web_root + "\n");
+        if (os == 'Darwin' || os == 'Linux') {
+            fs.symlinkSync(__dirname + init_script, init_dir + init_script);
+            stdout.write("Next time you reboot, Draughtsman will be at your service at port 3400 on your localhost, processing files in " + web_root + "\n");
+        } else {
+            stdout.write("OS not recognized. Could not configure Draughtsman to load at startup.");
+        }
     }
     stdout.write("Draughtsman has been successfully installed. You can run it from your shell using the `draughtsman` command.");
 }
